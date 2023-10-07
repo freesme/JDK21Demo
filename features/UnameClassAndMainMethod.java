f40cb58349dbe1a67113950bc840d9cbe5d2cdd0
Krane <freesme@163.com>
2023-10-07 09:56:10 +0800

    未命名类和实例主要方法

---
+++ features/UnameClassAndMainMethod.java
@@ -0,0 +1,12 @@
+/**
+ * 未命名类和实例main
+ * <p>
+ * 这是一个预览语言功能，默认禁用
+ * 使用 编译程序并使用;javac --release 21 --enable-preview xxx.java运行它 java --enable-preview xxx
+ * 或者 使用源代码启动器时，运行程序java --source 21 --enable-preview xxx.java
+ */
+public class UnameClassAndMainMethod {
+    void main() {
+        System.out.println("This UnameClassAndMainMethod");
+    }
+}
